0bd077067b2466804895be889e1267c76b674f39
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-04-12 17:15:47 +0000

    added html css js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+'use strict'
+
+function calculate () {
+  // input
+  const fahrenheit = parseInt(document.getElementById('fahrenheit').value)
+
+  // process
+  const celsius = (fahrenheit - 32) * 5 / 9 
+
+  // output
+  document.getElementById('celsius').innerHTML = 'Temperature in celsius : ' + celsius.toFixed(2) 
+}
